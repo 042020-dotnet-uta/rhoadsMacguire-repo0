d144522ee154db9fe2a913a1be0f0812c2ce0698
rhoads061 <rhoads.macguire@gmail.com>
2020-04-21 15:56:55 -0500

    adds my hello world program

---
+++ hello/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace hllo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int result = addPlusOne(3,4);
+            Console.WriteLine("Hello  World!");
+            Console.WriteLine(result);
+        }
+        public static int addPlusOne(int a, int b)
+        {
+            a++;
+            int c = a+b;
+            return c;
+
+        }}
+}
